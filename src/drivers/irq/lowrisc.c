4e0d2fe39b7c7a5b8320463ede6f3934cc6bedec
Matt Harvey <mattharvey@google.com>
2021-09-03 19:23:17 +0000

    Revert "Stops clearing PLIC hart0 registers on seL4 boot"
    This reverts commit be8b3750d5dd436aa915ae9e7d102634436b932a.
    
    Reason for revert: Causes spurious interrupt followed by loss of control of DebugConsole.
    
    Change-Id: I3298cffd2032157a6e0c8a8f5950d3f7897dbfa1

--- src/drivers/irq/lowrisc.c
+++ src/drivers/irq/lowrisc.c
@@ -4,25 +4,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-static void plic_clear_target_registers(uint32_t target) {
-  mmio_region_t base_addr = plic_base_addr();
-
-  // Clears interrupt enable registers.
-  ptrdiff_t offset = plic_irq_enable_base_for_target(target);
-  for (int i = 0; i < RV_PLIC_IE0_MULTIREG_COUNT; ++i) {
-    ptrdiff_t multireg_offset = offset + (i * sizeof(uint32_t));
-    mmio_region_write32(base_addr, multireg_offset, 0);
-  }
-
-  // Clears threshold registers.
-  offset = plic_threshold_base_for_target(target);
-  mmio_region_write32(base_addr, offset, 0);
-
-  // Clears software interrupt registers.
-  offset = plic_software_irq_base_for_target(target);
-  mmio_region_write32(base_addr, offset, 0);
-}
-
 static void plic_reset(void) {
   mmio_region_t base_addr = plic_base_addr();
 
@@ -38,6 +19,22 @@ static void plic_reset(void) {
     mmio_region_write32(base_addr, offset, 0);
   }
 
-  // Clears the target-related registers for the seL4 hart.
-  plic_clear_target_registers(RV_PLIC_SEL4_TARGET);
+  // Clears all of the target related registers.
+  for (dif_plic_target_t target = 0; target < RV_PLIC_PARAM_NUM_TARGET;
+       ++target) {
+    // Clears interrupt enable registers.
+    ptrdiff_t offset = plic_irq_enable_base_for_target(target);
+    for (int i = 0; i < RV_PLIC_IE0_MULTIREG_COUNT; ++i) {
+      ptrdiff_t multireg_offset = offset + (i * sizeof(uint32_t));
+      mmio_region_write32(base_addr, multireg_offset, 0);
+    }
+
+    // Clears threshold registers.
+    offset = plic_threshold_base_for_target(target);
+    mmio_region_write32(base_addr, offset, 0);
+
+    // Clears software interrupt registers.
+    offset = plic_software_irq_base_for_target(target);
+    mmio_region_write32(base_addr, offset, 0);
+  }
 }
