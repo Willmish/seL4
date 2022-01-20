57e47fd0d1b1f7b920e2ff44cffb064abc9daf1a
Wojciech Sipak <wsipak@antmicro.com>
2022-01-20 21:23:43 +0100

    add configuration options to shodan
    This provides options used by rootserver.cmake file
    so that we can build sel4test.
    
    Signed-off-by: Wojciech Sipak <wsipak@antmicro.com>
    
    Change-Id: I9eecf788a99132ae1d928a94b80747e72d848219

--- src/plat/shodan/config.cmake
+++ src/plat/shodan/config.cmake
@@ -6,6 +6,8 @@ if(KernelPlatformShodan)
     declare_seL4_arch(riscv32)
     config_set(KernelRiscVPlatform RISCV_PLAT "shodan")
     config_set(KernelPlatformFirstHartID FIRST_HART_ID 1)
+    config_set(KernelOpenSBIPlatform OPENSBI_PLATFORM "generic")
+    config_set(UseRiscVBBL RISCV_BBL OFF)
     list(APPEND KernelDTSList "tools/dts/shodan.dts")
     include_directories("${CMAKE_BINARY_DIR}/opentitan-gen/include")
     declare_default_headers(
