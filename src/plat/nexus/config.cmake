e674329dd42f52b35319632648a3a0ce73d27c58
June Tate-Gans <jtgans@google.com>
2023-07-05 19:49:55 +0000

    nexus: Duplicate shodan platform to nexus
    Change-Id: I33f1faf16ad1e1085177032e34b2c2c30c18d27a

---
+++ src/plat/nexus/config.cmake
@@ -0,0 +1,28 @@
+cmake_minimum_required(VERSION 3.7.2)
+
+declare_platform(shodan KernelPlatformShodan PLAT_SHODAN KernelArchRiscV)
+
+if(KernelPlatformShodan)
+    declare_seL4_arch(riscv32)
+    config_set(KernelRiscVPlatform RISCV_PLAT "shodan")
+    config_set(KernelPlatformFirstHartID FIRST_HART_ID 1)
+    # Shodan has no SBI
+    config_set(UseRiscVOpenSBI RISCV_OPENSBI OFF)
+    config_set(UseRiscVBBL RISCV_BBL OFF)
+    list(APPEND KernelDTSList "tools/dts/shodan.dts")
+    include_directories("${CMAKE_BINARY_DIR}/opentitan-gen/include")
+    declare_default_headers(
+        # TODO(mattharvey): [rdtime_sync] When switching to ibex, rdtime will
+        # cause a trap, which will end up reading the OpenTitan timer. After
+        # implementing that, this can be set to any desired value, but until
+        # then it needs to be set to the rate at which the value returned by
+        # cpu1 rdtime is incremented.
+        TIMER_FREQUENCY 24000000
+
+        INTERRUPT_CONTROLLER drivers/irq/smc_plic.h
+        # Must be >= kTopMatchaPlicIrqIdLastSmc in top_matcha.h
+        PLIC_MAX_NUM_INT 32
+    )
+else()
+    unset(KernelPlatformFirstHartID CACHE)
+endif()
